bedf233fba8f5da9dfdaca9ce9b353be2287519e
Sam <89450430+samantalovecat@users.noreply.github.com>
2022-05-11 00:06:03 +0200

    v1 please work

---
+++ js/interest.js
@@ -0,0 +1,18 @@
+
+//sidenav open
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+//toggle dark mode
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
